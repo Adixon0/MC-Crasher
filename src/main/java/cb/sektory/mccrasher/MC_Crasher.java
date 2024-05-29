c988df40820dc575615861d612f389182eb3be8c
Adrian Iżykowski <adrianizykowski7@gmail.com>
2024-05-29 22:52:28 +0200

    Stworzenie podstaw pluginu

---
+++ src/main/java/cb/sektory/mccrasher/MC_Crasher.java
@@ -0,0 +1,17 @@
+package cb.sektory.mccrasher;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MC_Crasher extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
